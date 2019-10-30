aa377d96190f1a3072b4d389f1a0215d7e3daf3f
senanito85 <sysad85@gmail.com>
2019-10-30 11:21:48 +1100

    updated aws S3 script

--- aws_to_s3_bkp.sh
+++ aws_to_s3_bkp.sh
@@ -3,18 +3,18 @@
 echo "JMS Archival Started"
 
 # Defining variables
-SRCTFLDR=~/jmsbin/
-DSTFLDR=~/PycharmProjects/scraper/AWSSCRIPT/
+SRCTFLDR=/jmsbin/
+DSTFLDR=/PycharmProjects/scraper/AWSSCRIPT/
 today=$(date '+%Y-%m-%d')
 
 tar czvf $DSTFLDR/jmsarchive_$today.tar.gz  $SRCTFLDR
 echo "Archival Completed"
 
 # copy archive to S3 bucket
-aws s3 cp $DSTFLDR/*.tar.gz s3://buckets/cgl-backup/test/
+aws s3 cp $DSTFLDR/*.tar.gz s3://cgl-backup/
 
-# wait 30 sec 
-sleep 30
+# wait 60 sec to wait till file appears in S3 
+sleep 60
 
 # check if the archive is successfully delivered to AWS S3 bucket
 SRCFILE=$(basename DSTFLDR/jmsarchive_$today.tar.gz)
@@ -22,11 +22,11 @@ DSTFILE=$(aws s3 ls s3://cgl-filestore/jmsarchive_$today.tar.gz | awk '{print $4
 
 # if copied file equals to destination file, aka "copy job was successfull", the delete Source file. Otherwise create log entry on failed job.
 if [ $SRCFILE == $DSTFILE ]; then
-   # rm $SRCFILE
-   echo "Files are equal it will be removed"
+   rm $SRCFILE
+   echo "Files are equal it will be removed" > /var/log/weeklyarchiveal.log
 else
    echo "Copy job was unsuccessfull on $today" > /var/log/weeklyarchiveal.log
-   #echo "Copy job for jmsarchive_$today.tar.gz FAILED!" | mail -s "CGL AWS Backup Job Failed" sananib@cgl.com.au
+   # send an email line to be added
 fi
 
 exit 0
