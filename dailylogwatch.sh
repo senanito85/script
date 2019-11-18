c19ca54fb16c3c02c6e1a9d3557d7d581390953f
Senanito <36265361+senanito85@users.noreply.github.com>
2019-11-18 11:23:19 +1100

    Update dailylogwatch.sh

--- dailylogwatch.sh
+++ dailylogwatch.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+logwatch --detail Low --mailto sibrahimov@ast.com --service all --range today
 
-logwatch --detail Low --mailto sibrahimov@.com.au --service all --range today
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------------
+# Config File
+"/etc/logwatch/conf/logwatch.conf" 3L, 179C
+
+# Local configuration options go here (defaults are in /usr/share/logwatch/default.conf/logwatch.conf)
+MailFrom = logwatch@astrp01.ast.lan
+MailTo = sibrahimov@ast.com
 
 
